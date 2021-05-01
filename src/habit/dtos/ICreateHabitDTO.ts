66759480d55a0ed13e1ba9f2471a4f378611d268
Italo Lima <italolima534@gmail.com>
2021-05-01 14:38:43 -0300

    chore: updating property frequency to enum type

--- src/habit/dtos/ICreateHabitDTO.ts
+++ src/habit/dtos/ICreateHabitDTO.ts
@@ -1,10 +1,19 @@
+enum weekDay {
+  everySunday = 1,
+  everyMonday = 2,
+  everyTuesday = 3,
+  everyWednesday = 4,
+  everyThursday = 5,
+  everyFriday = 6,
+  everySaturday = 7,
+}
 export default interface ICreateHabitDTO {
   userId: string;
   name: string;
   description: string;
   reminderQuestion: string;
   color: string;
-  frequency: string;
+  frequency: weekDay;
   reminder: boolean;
   pontuation: number;
 }
