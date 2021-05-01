b5074fcfce6232ac38f2fab94bb861a437f044f3
marcosgenesis <marcogenesisof@gmail.com>
2021-05-01 14:29:51 -0300

    starting cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use('/files', expressStatic(uploadConfig.directory));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
