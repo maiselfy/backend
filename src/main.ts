852f6d70caa3a16ac541513d7c74448e5e9af17f
marcosgenesis <marcogenesisof@gmail.com>
2021-05-01 22:26:58 -0300

    FINALLY fixing cors error pt.2

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,9 @@ async function bootstrap() {
   app.use('/files', expressStatic(uploadConfig.directory));
   app.enableCors({
     origin: ['http://localhost:3000', 'https://app.maiself.com.br'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
   });
   await app.listen(3000);
 }
