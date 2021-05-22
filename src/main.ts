a5de0a130a09bc90ab66c2bfb8808c6f753157fe
marcosgenesis <marcogenesisof@gmail.com>
2021-05-21 23:13:16 -0300

    fix cors header

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:3000', 'https://app.maiself.com.br'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
   await app.listen(3333);
