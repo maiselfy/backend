46913ec9c3a62a9138eb7d1d44540a53f73046de
Italo Lima <italolima534@gmail.com>
2021-08-09 14:28:05 -0300

    fix: adding exception handling to the habit view service

--- src/modules/habit/services/createHabit.service.ts
+++ src/modules/habit/services/createHabit.service.ts
@@ -25,11 +25,7 @@ export default class CreateHabitService {
         where: { id: user_id },
       });
 
-<<<<<<< HEAD
       if (!user)
-=======
-      if (!buddy) {
->>>>>>> 60e40fda9a8ed7b4a7069a540564303d6c4cb856
         throw new HttpException(
           'It is not possible to perform the operation, as there is no corresponding registered user',
           HttpStatus.NOT_FOUND,
