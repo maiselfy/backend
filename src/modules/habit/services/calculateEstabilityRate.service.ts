f49eefd97430d13f02513c5afa24047684b7c759
Italo Lima <italolima534@gmail.com>
2021-11-17 11:00:20 -0300

    chore: removing consoles

--- src/modules/habit/services/calculateEstabilityRate.service.ts
+++ src/modules/habit/services/calculateEstabilityRate.service.ts
@@ -4,6 +4,8 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import Habit from '../infra/typeorm/entities/Habit';
 import User from 'src/modules/user/infra/typeorm/entities/User';
 import HabitDayCheck from '../infra/typeorm/entities/HabitDayCheck';
+import { addDays } from 'date-fns';
+import { intervalToDuration } from 'date-fns';
 
 @Injectable()
 export default class CalculateEstabilityRateService {
@@ -14,7 +16,13 @@ export default class CalculateEstabilityRateService {
     private daysCheckRepository: Repository<HabitDayCheck>,
   ) {}
 
+  datesChek: any[];
+  dates: any[];
+  intervals: any[];
   async execute({ userId, habitId }): Promise<any> {
+    this.datesChek = [];
+    this.dates = [];
+    this.intervals = [];
     try {
       const user = await this.usersRepository.findOne({
         where: { id: userId },
@@ -56,9 +64,37 @@ export default class CalculateEstabilityRateService {
         },
       });
 
-      const estabilityRate = (
-        checksForHabit[1] / differenceBetweenDays
-      ).toFixed(2);
+      checksForHabit[0].forEach(check => {
+        this.datesChek.push(check.date);
+      });
+
+      let date = new Date(firstRecord.date);
+
+      for (let i = 0; i <= differenceBetweenDays; i++) {
+        this.dates.push(date);
+        date = addDays(date, 1);
+      }
+
+      let interval;
+      for (let i = 0; i < this.datesChek.length; i++) {
+        if (i == this.datesChek.length - 1) {
+          interval = intervalToDuration({
+            start: this.datesChek[i],
+            end: today,
+          });
+        } else {
+          interval = intervalToDuration({
+            start: this.datesChek[i],
+            end: this.datesChek[i + 1],
+          });
+        }
+        this.intervals.push(interval);
+      }
+
+      const fm = Number(checksForHabit[1] / differenceBetweenDays);
+      const rf = Number(4 / differenceBetweenDays);
+
+      const estabilityRate = (fm / rf).toFixed(2);
       return estabilityRate;
     } catch (error) {
       if (error) throw error;
